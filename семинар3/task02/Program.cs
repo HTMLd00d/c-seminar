751c716b0ca454e8650c66dbe4a44820ed2b6c3e
d00dez <statik19@yandex.ru>
2022-12-08 21:22:12 +0500

    03 task

---
+++ семинар3/task02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Padre(int num)
+{
+    int num1 = 1;
+    while(num1<num)
+    {
+        int square = num1*num1;
+        Console.Write($"{num}  ");
+        Console.WriteLine(square);
+        num1++;
+    }
+    return num;
+}
+int result = Padre(number);
