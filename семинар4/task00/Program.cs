cf809cd64f81ad0b37f2a58bada2baca9a017f78
d00dez <statik19@yandex.ru>
2022-12-12 23:15:45 +0500

    03

---
+++ семинар4/task00/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    int sum = 1;
+    for (int i = 1; i < num; i++)
+    {
+
+        sum = sum * i;
+    }
+    return sum;
+}
+int sumNumbers = SumNumbers(number);
+
+Console.WriteLine($"Сумма чисел от единицы до А {sumNumbers}");
+
+
