30b80a94b4912da65e1fea936e38ea74ce6ff829
d00dez <statik19@yandex.ru>
2022-12-12 22:45:01 +0500

    01

---
+++ семинар4/task01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumberCounter(int num)
+{
+    int counter = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        counter++;
+    }
+    return counter;
+
+}
+int numberCounter = NumberCounter(number);
+Console.WriteLine($"{number} ->{NumberCounter(number)}");
