be67744edf6294de1bc8afeef83a766906c56461
d00dez <statik19@yandex.ru>
2022-12-13 00:06:30 +0500

    04

---
+++ семинар4/task04/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[8];
+
+void FillArray(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+FillArray(array);
+PrintArray(array);
