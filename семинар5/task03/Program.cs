bc2ab71c495f36fd95a6117a587acfd77fbc94ff
d00dez <statik19@yandex.ru>
2022-12-14 22:24:32 +0500

    03

---
+++ семинар5/task03/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int FindNum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 9 && arr[i] < 100)
+        sum++;
+    }
+    return sum;
+
+}
+int[] arr = CreateArrayRndInt(123, 10, 99);
+PrintArray(arr);
+int result = FindNum(arr);
+Console.WriteLine($"Количество {result}");
