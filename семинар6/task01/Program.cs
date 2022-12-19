18890215a10917172233265d6229e4e052f01759
d00dez <statik19@yandex.ru>
2022-12-19 21:39:26 +0500

    01

---
+++ семинар6/task01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите три числа");
+Console.Write("первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите три числа");
+Console.Write("первое число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите три числа");
+Console.Write("первое число ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+bool Triangle(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < a + b;
+}
+
+Console.WriteLine(Triangle(num1, num2, num3) ?"YES":"NO");
