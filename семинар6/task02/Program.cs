f91d848713dacbdf027e26022c8f13c5297aa3c4
d00dez <statik19@yandex.ru>
2022-12-19 22:23:09 +0500

    01

---
+++ семинар6/task02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string ConverToBin(int num)
+{
+    string result = string.Empty;
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2;
+    }
+    return result;
+}
+
+Console.WriteLine(ConverToBin(num));
