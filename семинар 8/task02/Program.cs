bba14f2ad5abe81464bc872725ad265da4900c18
d00dez <statik19@yandex.ru>
2022-12-25 21:23:10 +0500

    00-01

---
+++ семинар 8/task02/Program.cs
@@ -0,0 +1,46 @@
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j]}");
+        }
+        Console.WriteLine("]");
+    }
+}
+int[,] array2d = CreateMatrixRndInt(4, 4, 1, 9);
+PrintMatrix(array2d);
+
+int[,] ReplaceRows(int[,] arr)
+{
+    int[,] rows = new int[arr.GetLength(0), arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            rows[i, j] = arr[j, i];
+        }
+    }
+    return rows;
+}
+Console.WriteLine();
+ReplaceRows(array2d);
+Console.WriteLine();
+PrintMatrix(ReplaceRows(array2d));
