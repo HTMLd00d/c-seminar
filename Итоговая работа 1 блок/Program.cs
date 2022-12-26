ee3a67145e1cf1daf94b3464a49514280e234b1c
d00dez <statik19@yandex.ru>
2022-12-26 21:36:58 +0500

    closer

--- Итоговая работа 1 блок/Program.cs
+++ Итоговая работа 1 блок/Program.cs
@@ -1,16 +1,45 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив строк, длина которых меньше либо равна 3 символам.
 // Первоночальный массив вводится с клавиатуры, либо задаётся на старте выполнения алгоритма.
-
 Console.Write("Введите количество строк: ");
 int count = Convert.ToInt32(Console.ReadLine()); 
 string[] arrayStr = new string[count]; 
 for (int i = 0; i < count; i++)
 {
-    Console.Write("Введите строку  ", i + 1);
-    arrayStr[i] = Console.ReadLine();
+    Console.Write($"Введите строку  , {i + 1}");
+    arrayStr[i] = Console.ReadLine()!;
 }
-Console.WriteLine("Строки длиною меньше или равной 3 символам");
-for (int i = 0; i < count; i++)
+
+
+string[] NotMore3Symbols(string[] str)
 {
-    Console.WriteLine(arrayStr[i]);
+    int count = 0;
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+            count++;
+    }
+
+    string[] strLess4 = new string[count];
+    count = 0;
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+            strLess4[count] = str[i];
+            count++;
+        }
+    }
+
+    return strLess4;
+}
+
+//Формирование массива из строк, длина которых меньше, либо равна 3 символам
+string[] notMore3Symbols = NotMore3Symbols(arrayStr);
+
+//Вывод полученного массива в консоль
+for (int i = 0; i < notMore3Symbols.Length; i++)
+{
+    Console.WriteLine(notMore3Symbols[i]);
 }
