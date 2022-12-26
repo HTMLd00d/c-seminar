ae7f9f8201ba42ae64cbb10a608f3e8124235a3f
d00dez <statik19@yandex.ru>
2022-12-26 20:36:39 +0500

    close to victory

--- Итоговая работа 1 блок/Program.cs
+++ Итоговая работа 1 блок/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив строк, длина которых меньше либо равна 3 символам.
+// Первоночальный массив вводится с клавиатуры, либо задаётся на старте выполнения алгоритма.
+
+Console.Write("Введите количество строк: ");
+int count = Convert.ToInt32(Console.ReadLine()); 
+string[] arrayStr = new string[count]; 
+for (int i = 0; i < count; i++)
+{
+    Console.Write("Введите строку  ", i + 1);
+    arrayStr[i] = Console.ReadLine();
+}
+Console.WriteLine("Строки длиною меньше или равной 3 символам");
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine(arrayStr[i]);
+}
